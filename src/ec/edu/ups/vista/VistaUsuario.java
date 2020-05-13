db00c49478bcfaa7556e52be4b99a6875183c331
braya <braya@BRYEPZ>
2020-05-13 08:26:14 -0500

    Creacion de los metodos registrar, eliminar, buscar, etc. en la clase vista usurio

--- src/ec/edu/ups/vista/VistaUsuario.java
+++ src/ec/edu/ups/vista/VistaUsuario.java
@@ -6,6 +6,7 @@
 package ec.edu.ups.vista;
 
 import ec.edu.ups.modelo.Usuario;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -28,5 +29,37 @@ public class VistaUsuario {
         String correo=leer.next();
         String contraseña=leer.next();
         return new Usuario(cedula, nombre, apellido, correo, contraseña);
+    }
+    
+    public Usuario actualizarUsuario(){
+        
+        System.out.println("Ingrese cedula del usuario a actualizar");
+        String cedula=leer.next();
+        System.out.println("Ingrse los nuevos detos: Nombre, Apellido, Correo, Contraseña");
+        String nombre=leer.next();
+        String apellido=leer.next();
+        String correo=leer.next();
+        String contraseña=leer.next();
+        return new Usuario(cedula, nombre, apellido, correo, contraseña);
+    }
+    
+    public Usuario eliminarCliente(){
+        leer=new Scanner(System.in);
+        System.out.println("Ingrese cedula del usuario a eliminar");
+        String cedula=leer.next();
+        return new Usuario(cedula, null, null, null, null);
+    }
+    
+    public String buscarUsuario(){
+        leer=new Scanner(System.in);
+        System.out.println("Ingrese cedula del usuario a buscar");
+        String cedula=leer.next();
+        return cedula;
+    }
+    
+    public void listarUsuarios(List<Usuario> usuarios){
+        for (Usuario usuario : usuarios) {
+            System.out.println("Datos del usuario: "+usuario);
+        }
     }
 }
