59cbf21b1e34cfd1b1c5c2d403e92d1ecc2efdef
braya <braya@BRYEPZ>
2020-05-13 12:54:10 -0500

    Controlador DAO usuario

---
+++ src/ec/edu/ups/dao/UsuarioDAO.java
@@ -0,0 +1,68 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.ups.dao;
+
+import ec.edu.ups.idao.IUsuarioDAO;
+import ec.edu.ups.modelo.Usuario;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ *
+ * @author braya
+ */
+public class UsuarioDAO implements IUsuarioDAO{
+    private List<Usuario> listaUsuario;
+
+    public UsuarioDAO(){
+        listaUsuario=new ArrayList<>();                
+    }
+    
+    @Override
+    public void create(Usuario usuario) {
+        listaUsuario.add(usuario);
+    }
+
+    @Override
+    public Usuario read(String cedula) {
+        for (Usuario usuario : listaUsuario) {
+            if(usuario.getCedula()==cedula){
+                return usuario;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void update(Usuario usuario) {
+        for (int i = 0; i < listaUsuario.size(); i++) {
+            Usuario u = listaUsuario.get(i);
+            if(u.getCedula()==usuario.getCedula()){
+                listaUsuario.set(i, usuario);
+                break;
+            }
+        }
+    }
+
+    @Override
+    public void delete(Usuario usuario) {
+        Iterator<Usuario> it=listaUsuario.iterator();
+        while(it.hasNext()){
+            Usuario u=it.next();
+            if(u.getCedula()==usuario.getCedula()){
+                it.remove();
+                break;
+            }
+        }
+    }
+
+    @Override
+    public List<Usuario> findall() {
+        return listaUsuario;
+    }
+    
+}
